20c130df00e68ea9435f0f381f3c126d5fb0c837
Robert Norton <robertnorton913@gmail.com>
2024-11-13 17:52:34 +0100

    Style cards list

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import "./Footer.css";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
       <p className="footer__dev">Developed by Robert Norton</p>
-      <p className="footer__year">2024</p>
+      <p className="footer__year">{currentYear}</p>
     </footer>
   );
 }
